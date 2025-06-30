2f21a2e6588a6d90ddf44490df25acc494e33ee2
borislav <b.novikov@prosw-team.com>
2025-06-30 17:57:04 +0200

    fix: correct SQL filter syntax for trade status queries using is/isnot operators

--- app/crud.py
+++ app/crud.py
@@ -237,7 +237,9 @@ def get_trades(
     """Get all trades."""
     query = db.query(models.Trade)
     if is_closed is True:
-        query = query.filter(models.Trade.sell_date is not None)
+        # For closed trades, sell_date should not be NULL
+        query = query.filter(models.Trade.sell_date.isnot(None))
     elif is_closed is False:
-        query = query.filter(models.Trade.sell_date is None)
+        # For open trades, sell_date should be NULL
+        query = query.filter(models.Trade.sell_date.is_(None))
     return query.offset(skip).limit(limit).all()
